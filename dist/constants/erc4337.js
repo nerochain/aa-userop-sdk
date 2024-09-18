d3a5b3e33ed461874a7b3b248db78ef1e3c15242
jason <realskill@gmail.com>
2024-09-18 10:25:52 +0800

    change default rpc-url

--- dist/constants/erc4337.js
+++ dist/constants/erc4337.js
@@ -4,8 +4,8 @@ exports.ERC4337 = void 0;
 exports.ERC4337 = {
     EntryPoint: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789",
     SimpleAccount: {
-        Factory: "0x5B8D0D3089CbDf31D5e5E95EF3918A997E775aB1",
+        Factory: "0x9406Cc6185a346906296840746125a0E44976454",
     },
-    PaymasterRPC: "https://aa-demo.newhuoapps.com/api/paymaster",
-    BundlerRPC: "https://aa-demo.newhuoapps.com/api/bundler",
+    PaymasterRPC: "https://paymaster.nerochain.io",
+    BundlerRPC: "https://bundler.service.nerochain.io",
 };
